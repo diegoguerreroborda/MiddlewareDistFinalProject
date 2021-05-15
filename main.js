1b75c3c36586a5f1a278e0aa68205bc102c6d498
Diego Guerrero <diego.guerrero02@uptc.edu.co>
2021-05-15 15:07:47 -0500

    :v

--- main.js
+++ main.js
@@ -2,16 +2,21 @@ const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
 
-const PORT = 3050;
+const PORT = process.argv[2];
 
 const app = express()
 app.use(bodyParser.json())
 
+app.get('/', (req, res) => {
+    console.log('llega')
+    res.send(`Hola mundooo ${PORT}`)
+})
+
 app.post('/fileupload', (req, res) => {
     console.log(req)
-    res.sendStatus(200);
+    res.send(PORT);
 })
 
 app.listen(PORT, () => {
-    console.log(`Example app listening at http://172.17.0.1:${PORT}`)
+    console.log(`Example app listening at http://localhost:${PORT}`)
 })
